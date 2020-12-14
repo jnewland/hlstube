82379cd24585b386f6c624217a7c4f3a908bf3e7
Jesse Newland <jesse@jnewland.com>
2020-12-13 22:02:00 -0600

    400

--- hlstube.go
+++ hlstube.go
@@ -2,29 +2,56 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os/exec"
+	"strings"
 )
 
 type HLSTube struct {
-	paths   map[string]string
-	streams map[string]*Stream
+	m3us map[string]string
 }
 
 func (h *HLSTube) handler(w http.ResponseWriter, r *http.Request) {
-	// TODO streams instead
-	if h.paths[r.URL.Path] == "" {
-		// TODO concurrency limting of some sort?
-
-		fmt.Fprint(w, "starting stream")
-		// h.paths[r.URL.Path] = r.URL.Path
-		// cmd := exec.Command("bash", "-c", fmt.Sprintf("echo %s && sleep 5", r.URL.Path))
-		// err := cmd.Start()
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// log.Println(cmd.Process.Pid)
-	} else {
-		// TODO
-		http.FileServer(http.Dir("")).ServeHTTP(w, r)
+	// TODO do some additional sanitization
+	v := strings.Split(r.URL.Path, "/")[1]
+	if v == "favicon.ico" {
+		http.Error(w, fmt.Sprintf("%s not found", v), http.StatusNotFound)
+		return
 	}
+	log.Println(v)
+	if len(v) == 0 {
+		http.Error(w, fmt.Sprintf("%s not found", v), http.StatusNotFound)
+		return
+	}
+	if h.m3us[v] == "" {
+		m3u, err := exec.Command("youtube-dl", fmt.Sprintf("https://www.youtube.com/watch?v=%s", v), "-g").Output()
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		h.m3us[v] = strings.TrimSpace(string(m3u))
+		log.Println(h.m3us[v])
+	}
+
+	origin, err := url.Parse(h.m3us[v])
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	director := func(req *http.Request) {
+		req.URL.Host = origin.Host
+		req.URL.Scheme = origin.Scheme
+		req.URL.Path = origin.Path
+		req.URL.RawPath = origin.RawPath
+	}
+	proxy := &httputil.ReverseProxy{Director: director}
+
+	w.Header().Set("X-HLSTube", "is rad")
+	proxy.ServeHTTP(w, r)
 }
--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	hlstube := &HLSTube{}
-	path, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-	}
-	hlstube.paths = make(map[string]string)
+	hlstube.m3us = make(map[string]string)
 	http.HandleFunc("/", hlstube.handler)
 	http.ListenAndServe(":8080", nil)
 }
