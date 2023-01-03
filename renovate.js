32f47bf17db362216e4db88b73846a9fa382886d
Jesse Newland <jesse@jnewland.com>
2023-01-03 11:19:14 -0500

    use app token to workaround rate limit

--- renovate.js
+++ renovate.js
@@ -1,5 +1,13 @@
 module.exports = {
   globalExtends: ['github>jnewland/.github'],
+  hostRules: [
+    {
+      hostType: 'github',
+      matchHost: 'github.com',
+      username: process.env.RENOVATE_USERNAME,
+      token: process.env.RENOVATE_GITHUB_COM_TOKEN,
+    },
+  ],
   labels: ['renovate'],
   extends: ['config:base'],
   rebaseWhen: 'conflicted',
